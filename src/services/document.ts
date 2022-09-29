c5a71ad5b273381f1b51be4e8857523b6bbdfa9a
yong <tkddbs1644@gmail.com>
2022-09-29 19:46:06 +0900

    chore: api에 반환타입 추가

--- src/services/document.ts
+++ src/services/document.ts
@@ -1,28 +1,38 @@
 import Api from '~/core/api'
+import { IDocument } from '~/models/document'
 
 class DocumentApi extends Api {
-  async getAllDocument() {
+  async getAllDocument(): Promise<Pick<IDocument, 'id' | 'title' | 'documents'>[]> {
     return await this.request('/documents')
   }
 
-  async getDocument(documentId: number) {
+  async getDocument(documentId: number): Promise<IDocument> {
     return await this.request(`/documents/${documentId}`)
   }
 
-  async postNewDocument({ title, parentNodeId }: { title: string; parentNodeId: number }) {
+  async postNewDocument({
+    title,
+    parentNodeId,
+  }: {
+    title: string
+    parentNodeId: number
+  }): Promise<{ id: number; title: string; createdAt: string; updatedAt: string }> {
     return await this.request('/documents', {
       method: 'POST',
       body: JSON.stringify({ title, parent: parentNodeId }),
     })
   }
 
-  async removeDocument(documentId: number) {
+  async removeDocument(documentId: number): Promise<void> {
     return await this.request(`/documents/${documentId}`, {
       method: 'DELETE',
     })
   }
 
-  async editDocument(documentId: number, requestBodyObj: { title: string; content: string }) {
+  async editDocument(
+    documentId: number,
+    requestBodyObj: { title: string; content: string }
+  ): Promise<{ title: string; content: string }> {
     const { title, content } = requestBodyObj
     return await this.request(`/documents/${documentId}`, {
       method: 'PUT',
